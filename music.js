0ef4c6d2facef4987d0688ff67068fccf97e9aa0
colorhook <colorhook@gmail.com>
2014-04-04 18:27:16 +0800

    add music

--- music.js
+++ music.js
@@ -10,12 +10,12 @@ module.exports = function(webot) {
   var reg_music_cmd = /^(歌曲|听?歌|音乐|电台|radio|music|m\s+)\s*(.+)/i;
 
   function do_search(info, next) {
-      return {
-        title: '豆瓣电台',
-        description: '豆瓣FM是你专属的个性化音乐收听工具。它简单方便，打开就能收听。在收听过程中，你可以用“红心”、“垃圾桶”或者“跳过” 告诉豆瓣FM你的喜好。豆瓣FM将根据你的操作和反馈，从海量曲库中自动发现并播出符合你音乐口味的歌曲。',
-        picurl: 'http://img2.91huo.cn/mm/2012/06/21/dbfm.jpg',
-        url: 'http://douban.fm'
-      };
+    webot.reply({
+      title: '豆瓣电台',
+      description: '豆瓣FM是你专属的个性化音乐收听工具。它简单方便，打开就能收听。在收听过程中，你可以用“红心”、“垃圾桶”或者“跳过” 告诉豆瓣FM你的喜好。豆瓣FM将根据你的操作和反馈，从海量曲库中自动发现并播出符合你音乐口味的歌曲。',
+      picurl: 'http://img2.91huo.cn/mm/2012/06/21/dbfm.jpg',
+      url: 'http://douban.fm'
+    })
   }
 
   webot.set('music', {
